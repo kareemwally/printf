1a8a6e01ebce7dc0fad7ea2edb84834898744527
BENFADDOUL Hamza <99604220+Hamza-benfaddoul@users.noreply.github.com>
2023-10-17 08:13:50 +0100

    i create _stren fuction and update print_str and print_persentage
    because was a problem with print_presantage function and create our strlen function because it  not authorized to use buildin function

---
+++ _printf.c
@@ -0,0 +1,89 @@
+#include "_printf.h"
+/**
+ *print_ch-function
+ *Description:only prints character
+ *@c:the char we need to print
+ *Return:(void)
+ */
+void print_ch (char c)
+{
+write(1, &c, 1);
+}
+/**
+ *_strlen-function
+ *Description:count length of a string
+ *@str:the pointer to string to be counted
+ *Return:(lenght)
+ */
+size_t _strlen (const char *str)
+{
+size_t	len = 0;
+while (str[len])
+	len++;
+return (len);
+}
+/**
+ *print_str-function
+ *Description:only prints strings to stdout
+ *@st:string we need to print
+ *@count:total number of bytes to print
+ *Return:(void)
+ */
+void print_str (char *str, int *count)
+{
+size_t i;
+for (i = 0; i < _strlen(str); i++)
+{
+print_ch(str[i]);
+(*count)++;
+}
+}
+/**
+ *print_persentage-function
+ *Description:simple function to print %
+ *Return:(void)
+ */
+void print_persentage(void)
+{
+write(1, "%", 1);
+}
+/**
+ *_printf-function
+ *Description:this function is similar to printf
+ *@format:the stream we need to print to stdout
+ *Return:(int)
+ */
+int _printf(const char *format, ...)
+{
+size_t i; 
+int count = 0;
+va_list res;
+va_start(res, format);
+for (i = 0; i < _strlen(format); i++)
+{
+if (format[i] == '%' && format[i + 1] == 's')
+{
+print_str(va_arg(res, char *), &count);
+i++;
+}
+else if (format[i] == '%' && format[i + 1] == 'c')
+{
+print_ch(va_arg(res, int));
+count++;
+i++;
+}
+else if (format[i] == '%' && format[i + 1] == '%')
+{
+print_persentage();
+count++;
+i++;
+}
+else if (format[i] != '\0')
+{
+print_ch(format[i]);
+count++;
+}
+}
+va_end(res);
+return (count);
+}
---
+++ _printf.h
@@ -0,0 +1,9 @@
+#ifndef _printf_H
+#define _printf_H
+#include <stdio.h>
+#include<unistd.h>
+#include<stdarg.h>
+
+int _printf(const char *format, ...);
+
+#endif 
