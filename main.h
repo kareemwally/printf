a53c7341545b69fdd62ce088958c41a03e961b22
hamza benfaddoul <benfaddoul01@gmail.com>
2023-10-17 06:45:57 -0700

    update

--- main.h
+++ main.h
@@ -4,13 +4,11 @@
 #include<unistd.h>
 #include<stdarg.h>
 
-int		_printf(const char *format, ...);
-void	_puthexa_lower(unsigned int nbr, size_t *count);
-void	_puthexa_upper(unsigned int nbr, size_t *count);
-void	_putaddress(unsigned long int nbr, size_t *count);
+int	_printf(const char *format, ...);
 int	_putchar(char c);
 size_t	_putstr(char *str);
 void	_putnbr(int nbr, size_t *count);
 size_t	_strlen(const char *str);
+void	_putnbr_base(long nbr, char *base, size_t *count);
 
 #endif
